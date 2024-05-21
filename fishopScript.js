d0d9cbeb693f1270515ce84406dc02865aef903e
Perdonator <153538056+Perdonator@users.noreply.github.com>
2024-05-21 07:57:05 +0200

    Delete fishopScript.js

--- fishopScript.js
+++
@@ -1,7 +0,0 @@
-let coin = localStorage.getItem("Coins") || 1;
-function skinChange() {
-	coin = coin - 5;
-	document.getElementById("fishKing").src = "fishskin.jpg";
-	localStorage.setItem("Coins", coin);
-
-}
